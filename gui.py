ac7109e215e3db95efcf0a231ce67ed5f1378bf2
jeanotx32 <59669781+jeanotx32@users.noreply.github.com>
2024-09-24 19:18:50 -0400

    guiV1

--- gui.py
+++ gui.py
@@ -1,10 +1,13 @@
 from tkinter import *
 from tkinter import ttk
-
+import sys
+import pytguess as pg
 
 root = Tk()
-frm = ttk.Frame(root, padding=10)
+frm = ttk.Frame(root, padding=20)
 frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+ttk.Label(frm, text="Phasmophobot").grid(column=0, row=0)
+ttk.Button(frm, text="Test2", command=pg.test2).grid(column=2, row=2)
+ttk.Button(frm, text="Start Game Loop", command=pg.start).grid(column=3, row=2)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=10, row=2)
 root.mainloop()
